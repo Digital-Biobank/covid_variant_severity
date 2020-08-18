38acda27276eebd443849a08d7d20c8540d5ee76
Martin Skarzynski <marskar@gmail.com>
2020-08-18 14:50:33 -0400

    add script that randomizes data

---
+++ random.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# %% Read in data random rows
+df = pd.read_parquet("00_77142-vcf_wide.parquet").fillna(0)
+
+# %% Randomize rows
+random_rows = df.sample(
+    frac=1,
+    random_state=42,
+    axis="index"
+).set_axis(df.index, axis="index")
+
+# %% Randomize columns
+random_cols = random_rows.sample(
+    frac=1,
+    random_state=42,
+    axis="columns"
+).set_axis(df.columns, axis="columns")
+
+# %% Save randomized data
+random_cols.to_parquet("00_77142-vcf_wide_random.parquet")
+random_cols.to_csv("00_77142-vcf_wide_random.csv")
